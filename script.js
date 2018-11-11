d3061d935cf3cb4d780ca9d99b93cca30bcd2f9a
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-11-11 10:03:16 -0200

    refactoring functions

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.onload = function() {
-fetch('http://trampos.co/api/oportunidades.json')
- .then(response => response.json())
- .then(data => console.log(data))
+  function chamada(){
+    let data =  fetch('http://trampos.co/api/oportunidades.json')
+     .then(response => response.json())
+     .then(data => {
+       return data;
+      })
+     .catch(error => {
+       console.error(error);
+      });
+      return data;
+  }
+  const resposta = chamada();
+  console.table(resposta);
+  function template(){
+    
+  }
+
+
 }
