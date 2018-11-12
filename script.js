45760a03498f2d2ff8e039addc06e76c179569e9
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-11-12 18:40:05 -0200

    Refatorando codigo

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 window.onload = function() {
   function chamada(){
     let data =  fetch('http://trampos.co/api/oportunidades.json')
-     .then(response => response.json())
-     .then(data => {
-       return data;
+      .then((response) => response.json())
+      .then((responseData) => {
+        let template =
+        `<table>
+         <th class="">Vagas Trampos</th>
+        `;
+        const resposta = responseData;
+        resposta.map(function(resposta) {
+        template +=`<tr><td class="">${resposta.opportunity.name}</td></tr>`;
+
+
+
+
+
+          console.log(resposta.opportunity.name);
+        });
+        template +=
+       `<th class="">Vagas Trampos</th>
+       </table>
+       `;
+        const widget = document.querySelector("#widget");
+        widget.innerHTML = template;
       })
-     .catch(error => {
-       console.error(error);
-      });
-      return data;
-  }
-  const resposta = chamada();
-  console.table(resposta);
-  function template(){
-    
-  }
+      .catch((error) => { console.warn(error); });
 
-
+  }
+  chamada();
 }
